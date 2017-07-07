dd726d101ca5777c4dd97443679d9c3ba14aaac1
ömer öner <onerromerr@gmail.com>
2017-07-07 11:53:08 +0300

    Update userlist.sh

--- scripts/bash/userlist.sh
+++ scripts/bash/userlist.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-host1=$(hostname)
-host2=$(hostname -i)
+server='
+10.10.11.10
+10.10.11.11
+10.10.11.12
+10.10.11.13
+'
 
-userlist=$(cat /etc/passwd | awk -F':' '$3>=500 &&$3<=6000{ print $1}')
-echo $host1-$host2 users list: $userlist
+
+
+for host in $server
+do
+echo $host users list:
+#ssh root@$host  cat /etc/passwd | awk -F':' '$3>=500 &&$3<=60000{ print $1}'
+ssh root@$host  cat /etc/passwd | awk -F: '/\/home/ {printf "%s:%s\n",$1,$3}'
+echo -------------------------
+done
+
