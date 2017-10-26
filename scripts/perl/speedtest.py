a09c45bc1b9e9ace9d899da4850559e41fac6c84
ömer öner <onerromerr@gmail.com>
2017-10-26 16:59:31 +0300

    Update speedtest.py

--- scripts/perl/speedtest.py
+++ scripts/perl/speedtest.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# Copyright 2012-2016 Matt Martz
+# All Rights Reserved.
+#
 #    Licensed under the Apache License, Version 2.0 (the "License"); you may
 #    not use this file except in compliance with the License. You may obtain
 #    a copy of the License at
